86060b7e480c823c9f854fc45d198d949115a9fe
Eoghan Crowley <eoghanscrowley@gmail.com>
2016-05-21 23:03:20 +0100

    add video and slide transitions

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
 $('#email-text').hide();
+$('.video-link').hide();
+$('#video').hide();
 $.fn.extend({
     animateCss: function (animationName) {
         var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
@@ -15,11 +17,22 @@ $('#banner-pic').mouseenter(function(){
 $('#banner-pic').click(function(){
 	var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
 	$(this).addClass('animated hinge').one(animationEnd, function() {
-            $(this).removeClass('animated hinge');
-            $(this).hide();
-            $('#email-text').show();
-            $('#email-text').animateCss('flipInX');
-        });
-
+        $(this).removeClass('animated hinge');
+        $(this).hide();
+        $('#email-text').show();
+        $('#email-text').animateCss('flipInX');
+        $('.video-link').show();
+        $('.video-link').animateCss('flipInX');
+    });
 });
 
+$('.video-link').click(function() {
+	
+	var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+    $('#landing-page').addClass('animated slideOutUp').one(animationEnd, function() {
+        $('#landing-page').removeClass('animated slideOutUp');
+        $('#landing-page').hide();
+        $('#video').show().animateCss('slideInUp');
+        // $('#video').show();
+    });
+});
