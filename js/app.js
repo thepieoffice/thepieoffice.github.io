a21b046904c9be24cd1d57957df69c1437528156
Eoghan Crowley <eoghanscrowley@gmail.com>
2016-05-24 01:34:54 +0100

    clean up code

--- js/app.js
+++ js/app.js
@@ -1,28 +1,48 @@
-$('#email-text').hide();
-$('.video-link').hide();
-$('.video-container-1').hide();
-$('.video-container-2').hide();
-$('.to-contact-page').hide();
-$('.contact-page').hide();
 var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
 
+// Create custom jquery methods for animations and page transitions
 $.fn.extend({
     animateCss: function (animationName) {  
         $(this).addClass('animated ' + animationName).one(animationEnd, function() {
             $(this).removeClass('animated ' + animationName);
         });
+    },
+    slideScreenRight: function (newScreen) {
+        $(this).addClass('animated slideOutLeft').one(animationEnd, function() {
+            $(this).removeClass('animated slideOutLeft').hide();
+        });
+        $(newScreen).show().animateCss('slideInRight');
+    },
+    slideScreenLeft: function (newScreen) {
+        $(this).addClass('animated slideOutRight').one(animationEnd, function() {
+            $(this).removeClass('animated slideOutRight').hide();
+        });
+        $(newScreen).show().animateCss('slideInLeft');
+    },
+    slideScreenUp: function (newScreen) {
+        $(this).addClass('animated slideOutUp').one(animationEnd, function() {
+            $(this).removeClass('animated slideOutUp').hide();
+        });
+        $(newScreen).show().animateCss('slideInUp');
+    },
+    slideScreenDown: function (newScreen) {
+        $(this).addClass('animated slideOutDown').one(animationEnd, function() {
+            $(this).removeClass('animated slideOutDown').hide();
+        });
+        $(newScreen).show().animateCss('slideInDown');
     }
 });
 
+
+// LANDING PAGE BANNER PIC EVENT HANDLERS
 $('#banner-pic').mouseenter(function(){
 	$(this).animateCss('shake');
 });
-
 $('#banner-pic').click(function(){
 	$(this).addClass('animated hinge').one(animationEnd, function() {
         $(this).removeClass('animated hinge');
         $(this).hide();
-        $('#email-text').show().addClass('animated flipInX').one(animationEnd, function() {
+        $('#landing-page-caption').show().addClass('animated flipInX').one(animationEnd, function() {
             $(this).removeClass('animated flipInX');
             $('.video-link').show().animateCss('animated flipInX');
             $('.to-contact-page').show().animateCss('animated flipInX');
@@ -31,51 +51,26 @@ $('#banner-pic').click(function(){
     });
 });
 
+
+// PAGE REDIRECTIONS
 $('.to-contact-page').click(function() {
-    $('#landing-page').addClass('animated slideOutLeft').one(animationEnd, function() {
-        $('#landing-page').removeClass('animated slideOutLeft').hide();
-    });
-    $('.contact-page').show().animateCss('slideInRight');
+    $('#landing-page').slideScreenRight('.contact-page');
 });
-
 $('.back-from-contact').click(function(){
-    $('.contact-page').addClass('animated slideOutRight').one(animationEnd, function() {
-        $('.contact-page').removeClass('animated slideOutRight').hide();       
-    });
-    $('#landing-page').show().animateCss('slideInLeft');
+    $('.contact-page').slideScreenLeft('#landing-page');
 });
-
 $('.video-link').click(function() {
-    $('#landing-page').addClass('animated slideOutUp').one(animationEnd, function() {
-        $('#landing-page').removeClass('animated slideOutUp').hide();
-    });
-    $('.video-container-1').show().animateCss('slideInUp');
+    $('#landing-page').slideScreenUp('.video-container-1');
 });
-
 $('.to-landing-page').click(function(){
-    $('.video-container-1').addClass('animated slideOutDown').one(animationEnd, function() {
-        $('.video-container-1').removeClass('animated slideOutDown').hide();
-    });
-    $('#landing-page').show().animateCss('slideInDown');
+    $('.video-container-1').slideScreenDown('#landing-page');
 });
-
 $('.to-video-2').click(function() {
-    $('.video-container-1').addClass('animated slideOutUp').one(animationEnd, function() {
-        $('.video-container-1').removeClass('animated slideOutUp').hide();
-    });
-    $('.video-container-2').show().animateCss('slideInUp');
+    $('.video-container-1').slideScreenUp('.video-container-2');
 });
-
 $('.to-video-1').click(function(){
-    $('.video-container-2').addClass('animated slideOutDown').one(animationEnd, function() {
-        $('.video-container-2').removeClass('animated slideOutDown').hide();
-    });
-    $('.video-container-1').show().animateCss('slideInDown');
+    $('.video-container-2').slideScreenDown('.video-container-1');
 });
-
 $('.to-contact-page-from-vids').click(function() {
-    $('.video-container-2').addClass('animated slideOutLeft').one(animationEnd, function() {
-        $('.video-container-2').removeClass('animated slideOutLeft').hide();
-    });
-    $('.contact-page').show().animateCss('slideInRight');
+    $('.video-container-2').slideScreenRight('.contact-page');
 });
